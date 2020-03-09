4717111fb9abdebc71ee21833a6e809019767e40
chandrakant <chandrakantsavvy@gmail.com>
2020-03-09 10:45:32 +0530

    added model class

---
+++ app/src/main/java/com/ppstudios/recyclerviewdemo/models/RecepieModel.java
@@ -0,0 +1,28 @@
+package com.ppstudios.recyclerviewdemo.models;
+
+public class RecepieModel {
+
+    private int img;
+    private String text;
+
+    public RecepieModel(int img, String text) {
+        this.img = img;
+        this.text = text;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
